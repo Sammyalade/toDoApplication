4bb73b1da1796b483a2ce3e88c97318bdb4ef5ff
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-06 00:12:54 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/datas/models/Task.java
+++ src/main/java/africa/semicolon/toDoApplication/datas/models/Task.java
@@ -1,4 +1,22 @@
 package africa.semicolon.toDoApplication.datas.models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name="task_table")
 public class Task {
+    @Id
+    private int id;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private Status status;
 }
