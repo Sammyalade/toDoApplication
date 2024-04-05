ca539e04526fc60801feabc29084c13a5d373650
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-06 00:24:44 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/datas/models/User.java
+++ src/main/java/africa/semicolon/toDoApplication/datas/models/User.java
@@ -1,4 +1,21 @@
 package africa.semicolon.toDoApplication.datas.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="user_table")
 public class User {
+    @Id
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+    @OneToOne
+    private TaskList taskList;
+
 }
