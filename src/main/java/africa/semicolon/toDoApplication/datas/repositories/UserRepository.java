9564f7de8f61dba26cb43de5a4ed24a64e0c06ce
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-06 00:26:22 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/datas/repositories/UserRepository.java
+++ src/main/java/africa/semicolon/toDoApplication/datas/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package africa.semicolon.toDoApplication.datas.repositories;
 
-public class UserRepository {
+import africa.semicolon.toDoApplication.datas.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
