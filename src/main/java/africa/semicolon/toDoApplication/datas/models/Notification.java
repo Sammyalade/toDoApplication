9063b409041c60b188831a9e1fe90c016a72ec81
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-06 00:05:47 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/datas/models/Notification.java
+++ src/main/java/africa/semicolon/toDoApplication/datas/models/Notification.java
@@ -1,4 +1,21 @@
 package africa.semicolon.toDoApplication.datas.models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "notification_table")
 public class Notification {
+    @Id
+    private int id;
+    private String message;
+    private LocalDateTime time;
+    private boolean read;
 }
