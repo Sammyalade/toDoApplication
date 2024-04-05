465d2e0a9f58b1c5e4268ebf3e0caecebc31b7a0
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-06 00:24:22 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/datas/models/TaskList.java
+++ src/main/java/africa/semicolon/toDoApplication/datas/models/TaskList.java
@@ -1,4 +1,18 @@
 package africa.semicolon.toDoApplication.datas.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "task_list_table")
 public class TaskList {
+    @Id
+    private int id;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Task> tasks;
+
 }
