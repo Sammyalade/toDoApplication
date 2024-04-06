21e56b9e2eca7a39935827506a3e7727237c5a12
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-06 23:53:38 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/services/notificationService/NotificationServiceImpl.java
+++ src/main/java/africa/semicolon/toDoApplication/services/notificationService/NotificationServiceImpl.java
@@ -1,11 +1,20 @@
 package africa.semicolon.toDoApplication.services.notificationService;
 
+import africa.semicolon.toDoApplication.datas.models.Notification;
+import africa.semicolon.toDoApplication.datas.repositories.NotificationRepository;
 import africa.semicolon.toDoApplication.dtos.NotificationRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class NotificationServiceImpl implements NotificationService {
+    @Autowired
+    private NotificationRepository notificationRepository;
     @Override
-    public void sendNotificationForTask(NotificationRequest notificationRequest) {
-
+    public void createNotification(NotificationRequest notificationRequest) {
+        Notification notification = new Notification();
+        notification.setMessage(notificationRequest.getMessage());
+        notificationRepository.save(notification);
     }
 
     @Override
