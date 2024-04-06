e17345c43f393a5b386b7b35924eaf29ca607acb
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-06 23:53:55 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/services/taskList/TaskListServiceImpl.java
+++ src/main/java/africa/semicolon/toDoApplication/services/taskList/TaskListServiceImpl.java
@@ -1,4 +1,26 @@
 package africa.semicolon.toDoApplication.services.taskList;
 
+import africa.semicolon.toDoApplication.dtos.TaskListCreationRequest;
+import africa.semicolon.toDoApplication.dtos.TaskListUpdateRequest;
+
 public class TaskListServiceImpl implements TaskListService{
+    @Override
+    public void createTaskList(TaskListCreationRequest taskListCreationRequest) {
+
+    }
+
+    @Override
+    public void updateTaskList(TaskListUpdateRequest taskListUpdateRequest) {
+
+    }
+
+    @Override
+    public void getTaskListForUser(String username) {
+
+    }
+
+    @Override
+    public void deleteTaskList(String username) {
+
+    }
 }
