62322d56c905e2cc520f9f380b64b784a61db892
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-07 01:49:57 +0100

    Commit

--- src/test/java/africa/semicolon/toDoApplication/services/NotificationServiceTest.java
+++ src/test/java/africa/semicolon/toDoApplication/services/NotificationServiceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -21,9 +23,17 @@ public class NotificationServiceTest {
 
     @Test
     public void createNotification_notificationIsCreatedTest() {
-        NotificationRequest notificationRequest = new NotificationRequest();
-        notificationRequest.setMessage("Message");
-        notificationService.createNotification(notificationRequest);
+        notificationService.createNotification("message");
+        assertThat(notificationRepository.count(), is(1L));
+    }
+
+    @Test
+    public void readNotification_notificationIsReadTest() {
+        Notification notification = notificationService.createNotification("message");
+        notificationService.markNotificationAsRead(notification.getId());
+        Optional<Notification> retrievedNotification = notificationService.findById(notification.getId());
+        Notification readNotification = retrievedNotification.get();
+        assertThat(readNotification.isRead(), is(true));
         assertThat(notificationRepository.count(), is(1L));
     }
 }
