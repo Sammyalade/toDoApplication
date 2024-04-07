0eefe2bfa4ae0d7ac393ef3deb1c18e5e060d5af
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-07 02:08:55 +0100

    Commit

--- src/test/java/africa/semicolon/toDoApplication/services/NotificationServiceTest.java
+++ src/test/java/africa/semicolon/toDoApplication/services/NotificationServiceTest.java
@@ -2,7 +2,6 @@ package africa.semicolon.toDoApplication.services;
 
 import africa.semicolon.toDoApplication.datas.models.Notification;
 import africa.semicolon.toDoApplication.datas.repositories.NotificationRepository;
-import africa.semicolon.toDoApplication.dtos.NotificationRequest;
 import africa.semicolon.toDoApplication.services.notificationService.NotificationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
