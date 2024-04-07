409886d1b2f82fed4a3b089ac0067595084c17cd
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-07 03:57:18 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/dtos/TaskUpdateRequest.java
+++ src/main/java/africa/semicolon/toDoApplication/dtos/TaskUpdateRequest.java
@@ -1,4 +1,15 @@
 package africa.semicolon.toDoApplication.dtos;
 
+import africa.semicolon.toDoApplication.datas.models.Status;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class TaskUpdateRequest {
+
+    private int id;
+    private Status status;
+    private LocalDate dueDate;
+
 }
