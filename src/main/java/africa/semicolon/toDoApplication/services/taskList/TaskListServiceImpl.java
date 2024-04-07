614ad61ee310630afa51e37a3369a7a9b585e596
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-07 22:54:15 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/services/taskList/TaskListServiceImpl.java
+++ src/main/java/africa/semicolon/toDoApplication/services/taskList/TaskListServiceImpl.java
@@ -1,26 +1,62 @@
 package africa.semicolon.toDoApplication.services.taskList;
 
-import africa.semicolon.toDoApplication.dtos.TaskListCreationRequest;
-import africa.semicolon.toDoApplication.dtos.TaskListUpdateRequest;
+import africa.semicolon.toDoApplication.datas.models.Task;
+import africa.semicolon.toDoApplication.datas.models.TaskList;
+import africa.semicolon.toDoApplication.datas.repositories.TaskListRepository;
+import africa.semicolon.toDoApplication.dtos.TaskCreationInTaskListRequest;
+import africa.semicolon.toDoApplication.dtos.TaskUpdateInTaskListRequest;
+import africa.semicolon.toDoApplication.services.taskService.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+import static africa.semicolon.toDoApplication.utility.Mapper.checkIfListIsNull;
+
+@Service
 public class TaskListServiceImpl implements TaskListService{
-    @Override
-    public void createTaskList(TaskListCreationRequest taskListCreationRequest) {
 
+    @Autowired
+    private TaskListRepository taskListRepository;
+    @Autowired
+    private TaskService taskService;
+
+    @Override
+    public TaskList createTaskList() {
+        TaskList taskList = new TaskList();
+        taskListRepository.save(taskList);
+        return taskList;
     }
 
     @Override
-    public void updateTaskList(TaskListUpdateRequest taskListUpdateRequest) {
-
+    public List<Task> getTaskListForUser(String username) {
+        TaskList taskList = (TaskList) taskListRepository.findAll();
+        return taskList.getTasks();
     }
 
     @Override
-    public void getTaskListForUser(String username) {
-
+    public void createTaskInATaskList(TaskCreationInTaskListRequest taskCreationInTaskListRequest){
+        Task task = taskService.createTask(taskCreationInTaskListRequest);
+        Optional<TaskList> taskList = searchForTaskListById(taskCreationInTaskListRequest.getId());
+        if(taskList.isPresent()){
+            TaskList taskList1 = taskList.get();
+            taskList1.setTasks((List<Task>) checkIfListIsNull(taskList1.getTasks()));
+            taskList1.getTasks().add(task);
+            taskListRepository.save(taskList1);
+        }
     }
 
     @Override
-    public void deleteTaskList(String username) {
+    public Optional<TaskList> searchForTaskListById(long id) {
+        return taskListRepository.findById(id);
+    }
 
+    @Override
+    public void editTaskInTaskList(TaskUpdateInTaskListRequest taskUpdateInTaskListRequest) {
+        Optional<TaskList> taskList = taskListRepository.findById(taskUpdateInTaskListRequest.getTaskListId());
+        if(taskList.isPresent()){
+            taskService.updateTaskDueDate(taskUpdateInTaskListRequest);
+        }
     }
 }
