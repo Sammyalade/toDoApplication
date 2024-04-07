d356cdf67617f0932042bb21980a81e9a70a7028
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-07 02:06:07 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/dtos/TaskCreationRequest.java
+++ src/main/java/africa/semicolon/toDoApplication/dtos/TaskCreationRequest.java
@@ -1,4 +1,17 @@
 package africa.semicolon.toDoApplication.dtos;
 
+import africa.semicolon.toDoApplication.datas.models.Notification;
+import africa.semicolon.toDoApplication.datas.models.Status;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class TaskCreationRequest {
+
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private Status status;
+    private Notification notification;
 }
