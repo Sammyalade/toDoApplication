0f0e854b8ca2e655c0af6cee56df1ff1a1bfbfd7
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-07 10:08:54 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/services/taskService/TaskServiceImpl.java
+++ src/main/java/africa/semicolon/toDoApplication/services/taskService/TaskServiceImpl.java
@@ -1,14 +1,20 @@
 package africa.semicolon.toDoApplication.services.taskService;
 
+import africa.semicolon.toDoApplication.datas.models.Notification;
 import africa.semicolon.toDoApplication.datas.models.Task;
+import africa.semicolon.toDoApplication.datas.repositories.NotificationRepository;
 import africa.semicolon.toDoApplication.datas.repositories.TaskRepository;
 import africa.semicolon.toDoApplication.dtos.TaskCreationRequest;
 import africa.semicolon.toDoApplication.dtos.TaskDeleteRequest;
+import africa.semicolon.toDoApplication.dtos.TaskNotificationTimeChangeRequest;
 import africa.semicolon.toDoApplication.dtos.TaskUpdateRequest;
 import africa.semicolon.toDoApplication.exception.EmptyStringException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Optional;
 
 import static africa.semicolon.toDoApplication.utility.Mapper.IsEmptyString;
@@ -18,10 +24,14 @@ import static africa.semicolon.toDoApplication.utility.Mapper.map;
 public class TaskServiceImpl implements TaskService {
     @Autowired
     private TaskRepository taskRepository;
+    @Autowired
+    private NotificationRepository notificationRepository;
     @Override
     public Task createTask(TaskCreationRequest taskCreationRequest) {
         if(IsEmptyString(taskCreationRequest.getTitle())) throw new EmptyStringException("Title cannot be empty");
         Task task = map(taskCreationRequest);
+        Notification notification = task.getNotification();
+        notificationRepository.save(notification);
         taskRepository.save(task);
         return task;
     }
@@ -44,16 +54,26 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public void updateTaskDueDate(TaskUpdateRequest taskUpdateRequest) {
+        updateTaskNotificationTime(map(taskUpdateRequest.getId(), taskUpdateRequest.getDueDate()));
         Optional<Task> task = searchForTaskById(taskUpdateRequest.getId());
         if(task.isPresent()) {
             task.get().setDueDate(taskUpdateRequest.getDueDate());
+
             taskRepository.save(task.get());
         }
     }
 
     @Override
-    public void updateTaskNotification(TaskUpdateRequest taskUpdateRequest) {
+    public void updateTaskNotificationTime(TaskNotificationTimeChangeRequest taskNotificationTimeChangeRequest) {
+        Optional<Task> task = searchForTaskById(taskNotificationTimeChangeRequest.getId());
+        if(task.isPresent()) {
+            LocalDateTime dateTime = task.get().getNotification().getTime();
+            LocalTime time = dateTime.toLocalTime();
+            dateTime = LocalDateTime.of(taskNotificationTimeChangeRequest.getTime(), time);
+            task.get().getNotification().setTime(dateTime);
 
+            taskRepository.save(task.get());
+        }
     }
 
     @Override
