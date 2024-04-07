e36782228486c771de3681a1802c0d150884f8ad
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-07 23:44:58 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/services/taskList/TaskListService.java
+++ src/main/java/africa/semicolon/toDoApplication/services/taskList/TaskListService.java
@@ -3,6 +3,7 @@ package africa.semicolon.toDoApplication.services.taskList;
 import africa.semicolon.toDoApplication.datas.models.Task;
 import africa.semicolon.toDoApplication.datas.models.TaskList;
 import africa.semicolon.toDoApplication.dtos.TaskCreationInTaskListRequest;
+import africa.semicolon.toDoApplication.dtos.TaskSearchInTaskListRequest;
 import africa.semicolon.toDoApplication.dtos.TaskUpdateInTaskListRequest;
 
 import java.util.List;
@@ -13,9 +14,10 @@ public interface TaskListService {
     TaskList createTaskList();
     List<Task> getTaskListForUser(String username);
 
-    void createTaskInATaskList(TaskCreationInTaskListRequest taskCreationInTaskListRequest);
+    Task createTaskInATaskList(TaskCreationInTaskListRequest taskCreationInTaskListRequest);
 
     Optional<TaskList> searchForTaskListById(long id);
 
     void editTaskInTaskList(TaskUpdateInTaskListRequest taskUpdateInTaskListRequest);
+    Task searchForTaskById(TaskSearchInTaskListRequest taskSearchInTaskListRequest);
 }
