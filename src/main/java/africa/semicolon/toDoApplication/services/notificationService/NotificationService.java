e08e6bb853423884cd2db326596cf040bd97d468
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-07 02:06:27 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/services/notificationService/NotificationService.java
+++ src/main/java/africa/semicolon/toDoApplication/services/notificationService/NotificationService.java
@@ -1,7 +1,6 @@
 package africa.semicolon.toDoApplication.services.notificationService;
 
 import africa.semicolon.toDoApplication.datas.models.Notification;
-import africa.semicolon.toDoApplication.dtos.NotificationRequest;
 
 import java.util.Optional;
 
