e3967b3f349277beedc5507f3ac48f06482648c7
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-08 13:36:56 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/contollers/UserController.java
+++ src/main/java/africa/semicolon/toDoApplication/contollers/UserController.java
@@ -1,4 +1,31 @@
 package africa.semicolon.toDoApplication.contollers;
 
+import africa.semicolon.toDoApplication.dtos.UserRegistrationRequest;
+import africa.semicolon.toDoApplication.dtos.response.UserApiResponse;
+import africa.semicolon.toDoApplication.dtos.response.UserRegistrationResponse;
+import africa.semicolon.toDoApplication.exception.TodoApplicationException;
+import africa.semicolon.toDoApplication.services.userService.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody UserRegistrationRequest userRegistrationRequest) {
+        try{
+            UserRegistrationResponse response = userService.registerUser(userRegistrationRequest);
+            return new ResponseEntity<>(new UserApiResponse(true, response), CREATED);
+        }
+        catch(TodoApplicationException e){
+            return new ResponseEntity<>(new UserApiResponse(false, e.getMessage()), BAD_REQUEST);
+        }
+    }
 }
