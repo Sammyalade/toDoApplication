a65b460ffd5020e5cca2fda30ae7abe07d43b688
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-08 13:40:56 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/dtos/UserRegistrationRequest.java
+++ src/main/java/africa/semicolon/toDoApplication/dtos/UserRegistrationRequest.java
@@ -1,4 +1,11 @@
 package africa.semicolon.toDoApplication.dtos;
 
+import lombok.Data;
+
+@Data
 public class UserRegistrationRequest {
+
+    private String username;
+    private String email;
+
 }
