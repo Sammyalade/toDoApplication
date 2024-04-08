970438e6c552d97a42742602b29f0c7693f2b589
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-08 13:42:07 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/services/userService/UserServiceImpl.java
+++ src/main/java/africa/semicolon/toDoApplication/services/userService/UserServiceImpl.java
@@ -1,53 +1,62 @@
 package africa.semicolon.toDoApplication.services.userService;
 
 import africa.semicolon.toDoApplication.datas.models.User;
+import africa.semicolon.toDoApplication.datas.repositories.UserRepository;
 import africa.semicolon.toDoApplication.dtos.*;
+import africa.semicolon.toDoApplication.dtos.response.UserRegistrationResponse;
+import africa.semicolon.toDoApplication.services.taskList.TaskListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.Optional;
 
+import static africa.semicolon.toDoApplication.utility.Mapper.map;
+
+@Service
 public class UserServiceImpl implements UserService {
-    @Override
-    public void registerUser(UserRegistrationRequest userRegistrationRequest) {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserRegistrationResponse registerUser(UserRegistrationRequest userRegistrationRequest) {
+        User user = map(userRegistrationRequest);
+        userRepository.save(user);
+        return map(user);
     }
 
     @Override
     public void loginUser(UserLoginRequest userLoginRequest) {
+        Optional<User> user = userRepository.findById(userLoginRequest.getId());
+        if (user.isPresent()) {
+            User user1 = user.get();
+            if(user1.getEmail().equals(userLoginRequest.getEmail())){
+                user1.setLocked(false);
+            }
+        }
 
     }
 
     @Override
-    public void logoutUser(String username) {
-
+    public void logoutUser(int id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            User user1 = user.get();
+            user1.setLocked(true);
+        }
     }
 
     @Override
     public void updateUser(UserUpdateRequest userUpdateRequest) {
-
+        Optional<User> user = userRepository.findById(userUpdateRequest.getId());
+        if (user.isPresent()) {
+            User user1 = user.get();
+            user1.setEmail(userUpdateRequest.getEmail());
+        }
     }
 
     @Override
-    public void deleteUser(UserDeleteRequest userDeleteRequest) {
-
-    }
-
-    @Override
-    public void changePassword(UserPasswordChangeRequest userPasswordChangeRequest) {
-
-    }
-
-    @Override
-    public void resetPassword(UserPasswordResetRequest userPasswordResetRequest) {
-
-    }
-
-    @Override
-    public void searchUser(String username) {
-
-    }
-
-    @Override
-    public List<User> getUsers() {
-        return null;
+    public void deleteUser(int id) {
+        Optional<User> user = userRepository.findById(id);
+        user.ifPresent(value -> userRepository.delete(value));
     }
 }
