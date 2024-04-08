546dc256a8facfd1e2b56184ce9a16ea6b0f862c
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-08 13:41:45 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/services/userService/UserService.java
+++ src/main/java/africa/semicolon/toDoApplication/services/userService/UserService.java
@@ -2,18 +2,16 @@ package africa.semicolon.toDoApplication.services.userService;
 
 import africa.semicolon.toDoApplication.datas.models.User;
 import africa.semicolon.toDoApplication.dtos.*;
+import africa.semicolon.toDoApplication.dtos.response.UserRegistrationResponse;
 
 import java.util.List;
 
 public interface UserService {
 
-    void registerUser(UserRegistrationRequest userRegistrationRequest);
+    UserRegistrationResponse registerUser(UserRegistrationRequest userRegistrationRequest);
     void loginUser(UserLoginRequest userLoginRequest);
-    void logoutUser(String username);
+    void logoutUser(int id);
     void updateUser(UserUpdateRequest userUpdateRequest);
-    void deleteUser(UserDeleteRequest userDeleteRequest);
-    void changePassword(UserPasswordChangeRequest userPasswordChangeRequest);
-    void resetPassword(UserPasswordResetRequest userPasswordResetRequest);
-    void searchUser(String username);
-    List<User> getUsers();
+
+    void deleteUser(int id);
 }
