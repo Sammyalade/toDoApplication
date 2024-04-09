b6f9f2f16ab24ce55cd73a008d166a92883bd9d8
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-09 22:26:18 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/dtos/TaskUpdateRequest.java
+++ src/main/java/africa/semicolon/toDoApplication/dtos/TaskUpdateRequest.java
@@ -5,16 +5,20 @@ import africa.semicolon.toDoApplication.datas.models.Status;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Data
 public class TaskUpdateRequest {
 
     private int id;
+    private int notificationId;
     private String title;
     private String description;
     private Status status;
     private Priority priority;
     private LocalDate dueDate;
+    private LocalTime time;
     private String message;
+    private boolean isRead;
 
 }
