261f449c1eee93a2ea8179b1e7326dc2f1558d48
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-09 16:53:08 +0100

    Commit

--- src/test/java/africa/semicolon/toDoApplication/services/NotificationServiceTest.java
+++ src/test/java/africa/semicolon/toDoApplication/services/NotificationServiceTest.java
@@ -2,8 +2,12 @@ package africa.semicolon.toDoApplication.services;
 
 import africa.semicolon.toDoApplication.datas.models.Notification;
 import africa.semicolon.toDoApplication.datas.repositories.NotificationRepository;
-import africa.semicolon.toDoApplication.dtos.NotificationTimeChangeRequest;
+import africa.semicolon.toDoApplication.dtos.NotificationUpdateRequest;
+import africa.semicolon.toDoApplication.exception.EmptyStringException;
+import africa.semicolon.toDoApplication.exception.IncorrectTimeException;
+import africa.semicolon.toDoApplication.exception.NotificationNotFoundException;
 import africa.semicolon.toDoApplication.services.notificationService.NotificationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,8 +15,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -24,6 +28,11 @@ public class NotificationServiceTest {
     @Autowired
     private NotificationRepository notificationRepository;
 
+    @BeforeEach
+    public void setUp() {
+        notificationRepository.deleteAll();
+    }
+
     @Test
     public void createNotification_notificationIsCreatedTest() {
         notificationService.createNotification("message");
@@ -33,7 +42,10 @@ public class NotificationServiceTest {
     @Test
     public void readNotification_notificationIsReadTest() {
         Notification notification = notificationService.createNotification("message");
-        notificationService.markNotificationAsRead(notification.getId());
+        NotificationUpdateRequest notificationUpdateRequest = new NotificationUpdateRequest();
+        notificationUpdateRequest.setId(notification.getId());
+        notificationUpdateRequest.setRead(true);
+        notificationService.updateNotification(notificationUpdateRequest);
         Notification retrievedNotification = notificationService.searchNotificationById(notification.getId());
         assertThat(retrievedNotification.isRead(), is(true));
         assertThat(notificationRepository.count(), is(1L));
@@ -42,11 +54,63 @@ public class NotificationServiceTest {
     @Test
     public void createNotification_changeTime_timeIsChangedTest(){
         Notification notification = notificationService.createNotification("message");
-        NotificationTimeChangeRequest notificationTimeChangeRequest = new NotificationTimeChangeRequest();
-        notificationTimeChangeRequest.setDateTime(LocalDateTime.of(LocalDate.parse("2021-12-12"), LocalTime.parse("09:00")));
-        notificationTimeChangeRequest.setId(notification.getId());
-        notificationService.changeTime(notificationTimeChangeRequest);
+        NotificationUpdateRequest notificationUpdateRequest = new NotificationUpdateRequest();
+        notificationUpdateRequest.setTime(LocalTime.parse("09:00"));
+        notificationUpdateRequest.setDate(LocalDate.parse("2021-12-12"));
+        notificationUpdateRequest.setId(notification.getId());
+        notificationService.updateNotification(notificationUpdateRequest);
         Notification retrievedNotification = notificationService.searchNotificationById(notification.getId());
         assertThat(retrievedNotification.getTime(), is(LocalDateTime.of(LocalDate.parse("2021-12-12"), LocalTime.parse("09:00"))));
+    }
+
+    @Test
+    public void searchForNotificationThatDoesNotExistTest(){
+        assertThatThrownBy(() -> {
+                    notificationService.searchNotificationById(11);
+                })
+                .isInstanceOf(NotificationNotFoundException.class)
+                .hasMessageContaining("Notification not found");
+    }
+
+    @Test
+    public void updateNotificationThatDoesNotExist_throwsExceptionTest(){
+        NotificationUpdateRequest notificationUpdateRequest = new NotificationUpdateRequest();
+        notificationUpdateRequest.setTime(LocalTime.parse("09:00"));
+        notificationUpdateRequest.setDate(LocalDate.parse("2021-12-12"));
+        notificationUpdateRequest.setId(11);
+        assertThatThrownBy(()->{
+            notificationService.updateNotification(notificationUpdateRequest);
+        })
+                .isInstanceOf(NotificationNotFoundException.class)
+                .hasMessageContaining("Notification not found");
+    }
+
+    @Test
+    public void createNotificationWithEmptyString_throwsExceptionTest(){
+        assertThatThrownBy(()->{
+            notificationService.createNotification("");
+        })
+                .isInstanceOf(EmptyStringException.class)
+                .hasMessageContaining("Message cannot be null or empty");
+    }
+    @Test
+    public void createNotificationWithNull_throwsExceptionTest(){
+        assertThatThrownBy(()->{
+            notificationService.createNotification(null);
+        })
+                .isInstanceOf(EmptyStringException.class)
+                .hasMessageContaining("Message cannot be null or empty");
+    }
+
+    @Test
+    public void createNotification_updateNotificationWithIncorrectDateFormat_throwsExceptionTest(){
+        Notification notification = notificationService.createNotification("message");
+        NotificationUpdateRequest notificationUpdateRequest = new NotificationUpdateRequest();
+        notificationUpdateRequest.setTime(LocalTime.parse("09"));
+        assertThatThrownBy(()->{
+            notificationService.updateNotification(notificationUpdateRequest);
+        })
+                .isInstanceOf(IncorrectTimeException)
+                .hasMessageContaining("You entered an incorrect time format");
     }
 }
