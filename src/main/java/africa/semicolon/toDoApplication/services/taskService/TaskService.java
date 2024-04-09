da2284a45911d676c4d29597b47100768b0a545d
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-09 23:00:01 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/services/taskService/TaskService.java
+++ src/main/java/africa/semicolon/toDoApplication/services/taskService/TaskService.java
@@ -2,13 +2,12 @@ package africa.semicolon.toDoApplication.services.taskService;
 
 import africa.semicolon.toDoApplication.datas.models.Task;
 import africa.semicolon.toDoApplication.dtos.request.TaskCreationRequest;
-import africa.semicolon.toDoApplication.dtos.request.TaskDeleteRequest;
 import africa.semicolon.toDoApplication.dtos.request.TaskUpdateRequest;
 
 public interface TaskService {
 
     Task createTask(TaskCreationRequest taskCreationRequest);
     void updateTask(TaskUpdateRequest taskUpdateRequest);
-    void deleteTask(TaskDeleteRequest taskDeleteRequest);
+    void deleteTask(int id);
     Task searchForTaskById(int id);
 }
