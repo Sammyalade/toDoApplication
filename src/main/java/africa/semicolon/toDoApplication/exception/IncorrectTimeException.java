9abc1681248886b7354916d7e06f3a316b98c950
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-09 22:26:33 +0100

    Commit

---
+++ src/main/java/africa/semicolon/toDoApplication/exception/IncorrectTimeException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.toDoApplication.exception;
+
+public class IncorrectTimeException extends TodoApplicationException{
+    public IncorrectTimeException(String message) {
+        super(message);
+    }
+}
