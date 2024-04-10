9d1b636e9fa18ab310380fb961ed47fd1bda1cd0
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-10 10:57:30 +0100

    Commit

--- src/test/java/africa/semicolon/toDoApplication/services/UserServiceTest.java
+++ src/test/java/africa/semicolon/toDoApplication/services/UserServiceTest.java
@@ -1,20 +1,27 @@
 package africa.semicolon.toDoApplication.services;
 
+import africa.semicolon.toDoApplication.datas.models.TaskList;
 import africa.semicolon.toDoApplication.datas.repositories.UserRepository;
 import africa.semicolon.toDoApplication.dtos.request.UserRegistrationRequest;
+import africa.semicolon.toDoApplication.dtos.request.UserTaskCreationRequest;
 import africa.semicolon.toDoApplication.dtos.response.UserRegistrationResponse;
 import africa.semicolon.toDoApplication.services.notificationService.NotificationService;
 import africa.semicolon.toDoApplication.services.taskList.TaskListService;
 import africa.semicolon.toDoApplication.services.taskService.TaskService;
 import africa.semicolon.toDoApplication.services.userService.UserService;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @SpringBootTest
+@Transactional
 public class UserServiceTest {
 
     @Autowired
@@ -44,7 +51,15 @@ public class UserServiceTest {
         userRegistrationRequest.setUsername("username");
         userRegistrationRequest.setEmail("email@email.com");
         UserRegistrationResponse user = userService.registerUser(userRegistrationRequest);
-        //userService.createTask(userTaskCreationRequest);
-
+        UserTaskCreationRequest userTaskCreationRequest = new UserTaskCreationRequest();
+        userTaskCreationRequest.setUserId(user.getUserId());
+        userTaskCreationRequest.setTitle("Title");
+        userTaskCreationRequest.setDescription("Description");
+        userTaskCreationRequest.setDueDate(LocalDate.now());
+        userTaskCreationRequest.setNotificationTime(LocalTime.now());
+        userService.createTask(userTaskCreationRequest);
+        assertThat(userRepository.count(), is(1L));
+        TaskList taskList = taskListService.searchForTaskList(user.getTaskListId());
+        assertThat(taskList.getTasks().size(),is(1));
     }
 }
