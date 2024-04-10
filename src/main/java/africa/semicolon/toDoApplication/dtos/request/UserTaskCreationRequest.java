bfa68eeaffa2137d0751bb7866974612c0adeb42
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-10 10:09:31 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/dtos/request/UserTaskCreationRequest.java
+++ src/main/java/africa/semicolon/toDoApplication/dtos/request/UserTaskCreationRequest.java
@@ -1,4 +1,18 @@
 package africa.semicolon.toDoApplication.dtos.request;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
 public class UserTaskCreationRequest {
+
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private String notificationMessage;
+    private LocalTime notificationTime;
+    private int taskListId;
+    private int userId;
 }
