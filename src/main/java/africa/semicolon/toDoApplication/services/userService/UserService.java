e54f4112e59287195d333c1de3028f8876660509
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-10 10:56:31 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/services/userService/UserService.java
+++ src/main/java/africa/semicolon/toDoApplication/services/userService/UserService.java
@@ -1,5 +1,6 @@
 package africa.semicolon.toDoApplication.services.userService;
 
+import africa.semicolon.toDoApplication.datas.models.User;
 import africa.semicolon.toDoApplication.dtos.request.*;
 import africa.semicolon.toDoApplication.dtos.response.TaskCreationResponse;
 import africa.semicolon.toDoApplication.dtos.response.UserRegistrationResponse;
@@ -8,6 +9,9 @@ public interface UserService {
 
     UserRegistrationResponse registerUser(UserRegistrationRequest userRegistrationRequest);
     TaskCreationResponse createTask(UserTaskCreationRequest userTaskCreationRequest);
+
+    User searchUserById(int userId);
+
     void loginUser(UserLoginRequest userLoginRequest);
     void logoutUser(int id);
     void updateUser(UserUpdateRequest userUpdateRequest);
