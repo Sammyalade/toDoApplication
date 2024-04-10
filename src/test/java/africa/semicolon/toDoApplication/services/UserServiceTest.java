9749802ffb73c467fd2849b3e6bf9ae054676fe9
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-10 23:17:17 +0100

    Commit

--- src/test/java/africa/semicolon/toDoApplication/services/UserServiceTest.java
+++ src/test/java/africa/semicolon/toDoApplication/services/UserServiceTest.java
@@ -1,13 +1,18 @@
 package africa.semicolon.toDoApplication.services;
 
+import africa.semicolon.toDoApplication.datas.models.Priority;
+import africa.semicolon.toDoApplication.datas.models.Status;
+import africa.semicolon.toDoApplication.datas.models.Task;
 import africa.semicolon.toDoApplication.datas.models.TaskList;
 import africa.semicolon.toDoApplication.datas.repositories.UserRepository;
 import africa.semicolon.toDoApplication.dtos.request.UserRegistrationRequest;
 import africa.semicolon.toDoApplication.dtos.request.UserTaskCreationRequest;
+import africa.semicolon.toDoApplication.dtos.request.UserTaskUpdateRequest;
+import africa.semicolon.toDoApplication.dtos.response.TaskCreationResponse;
 import africa.semicolon.toDoApplication.dtos.response.UserRegistrationResponse;
-import africa.semicolon.toDoApplication.services.notificationService.NotificationService;
+import africa.semicolon.toDoApplication.exception.EmptyStringException;
+import africa.semicolon.toDoApplication.exception.UserNotFoundException;
 import africa.semicolon.toDoApplication.services.taskList.TaskListService;
-import africa.semicolon.toDoApplication.services.taskService.TaskService;
 import africa.semicolon.toDoApplication.services.userService.UserService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
@@ -17,6 +22,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -28,10 +34,6 @@ public class UserServiceTest {
     private UserService userService;
     @Autowired
     private UserRepository userRepository;
-    @Autowired
-    private TaskService taskService;
-    @Autowired
-    private NotificationService notificationService;
     @Autowired
     private TaskListService taskListService;
 
@@ -55,11 +57,81 @@ public class UserServiceTest {
         userTaskCreationRequest.setUserId(user.getUserId());
         userTaskCreationRequest.setTitle("Title");
         userTaskCreationRequest.setDescription("Description");
+        userTaskCreationRequest.setNotificationMessage("Message");
         userTaskCreationRequest.setDueDate(LocalDate.now());
         userTaskCreationRequest.setNotificationTime(LocalTime.now());
         userService.createTask(userTaskCreationRequest);
         assertThat(userRepository.count(), is(1L));
         TaskList taskList = taskListService.searchForTaskList(user.getTaskListId());
         assertThat(taskList.getTasks().size(),is(1));
+    }
+
+    @Test
+    public void registerUserWithNullUsername_throwsNullException() {
+        UserRegistrationRequest userRegistrationRequest = new UserRegistrationRequest();
+        userRegistrationRequest.setUsername(null);
+        userRegistrationRequest.setEmail("email@email.com");
+        assertThatThrownBy(()->{
+            userService.registerUser(userRegistrationRequest);
+        })
+                .isInstanceOf(EmptyStringException.class)
+                .hasMessageContaining("Username cannot be null or empty");
+
+    }
+
+    @Test
+    public void registerUserWithEmptyStringUsername_throwsNullException() {
+        UserRegistrationRequest userRegistrationRequest = new UserRegistrationRequest();
+        userRegistrationRequest.setUsername("");
+        userRegistrationRequest.setEmail("email@email.com");
+        assertThatThrownBy(()->{
+            userService.registerUser(userRegistrationRequest);
+        })
+                .isInstanceOf(EmptyStringException.class)
+                .hasMessageContaining("Username cannot be null or empty");
+
+    }
+
+    @Test
+    public void registerUser_createTask_updateTask_taskIsUpdatedTest() {
+        UserRegistrationRequest userRegistrationRequest = new UserRegistrationRequest();
+        userRegistrationRequest.setUsername("username");
+        userRegistrationRequest.setEmail("email@email.com");
+        UserRegistrationResponse user = userService.registerUser(userRegistrationRequest);
+        UserTaskCreationRequest userTaskCreationRequest = new UserTaskCreationRequest();
+        userTaskCreationRequest.setUserId(user.getUserId());
+        userTaskCreationRequest.setTitle("Title");
+        userTaskCreationRequest.setDescription("Description");
+        userTaskCreationRequest.setNotificationMessage("Message");
+        userTaskCreationRequest.setDueDate(LocalDate.now());
+        userTaskCreationRequest.setNotificationTime(LocalTime.now());
+        TaskCreationResponse taskCreationResponse = userService.createTask(userTaskCreationRequest);
+        UserTaskUpdateRequest taskUpdateRequest = new UserTaskUpdateRequest();
+        taskUpdateRequest.setUserId(user.getUserId());
+        taskUpdateRequest.setTaskId(taskCreationResponse.getTaskId());
+        taskUpdateRequest.setTitle("New Title");
+        taskUpdateRequest.setDescription("New Description");
+        taskUpdateRequest.setRead(true);
+        taskUpdateRequest.setDueDate(LocalDate.now());
+        taskUpdateRequest.setMessage("New Message");
+        taskUpdateRequest.setPriority(Priority.HIGH_PRIORITY);
+        taskUpdateRequest.setStatus(Status.IN_PROGRESS);
+        taskUpdateRequest.setTime(LocalTime.now());
+        taskUpdateRequest.setNotificationId(taskCreationResponse.getNotificationId());
+        userService.updateTask(taskUpdateRequest);
+        Task task = taskListService.searchForTaskList(user.getTaskListId()).getTasks().get(0);
+        assertThat(task.getTitle(), is("New Title"));
+        assertThat(task.getStatus(), is(Status.IN_PROGRESS));
+        assertThat(task.getDueDate(), is(LocalDate.now()));
+        assertThat(task.getPriority(), is(Priority.HIGH_PRIORITY));
+    }
+
+    @Test
+    public void searchForUserThatDoesNotExist_throwsException() {
+        assertThatThrownBy(()-> {
+            userService.searchUserById(11);
+        })
+                .isInstanceOf(UserNotFoundException.class)
+                .hasMessageContaining("User not found");
     }
 }
