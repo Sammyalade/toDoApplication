589b7cbf8b699d61b7467b3c847cb118f4605239
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-11 11:12:06 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/contollers/UserController.java
+++ src/main/java/africa/semicolon/toDoApplication/contollers/UserController.java
@@ -1,16 +1,17 @@
 package africa.semicolon.toDoApplication.contollers;
 
-import africa.semicolon.toDoApplication.dtos.request.UserRegistrationRequest;
-import africa.semicolon.toDoApplication.dtos.response.UserApiResponse;
-import africa.semicolon.toDoApplication.dtos.response.UserRegistrationResponse;
+import africa.semicolon.toDoApplication.datas.models.Task;
+import africa.semicolon.toDoApplication.dtos.request.*;
+import africa.semicolon.toDoApplication.dtos.response.*;
 import africa.semicolon.toDoApplication.exception.TodoApplicationException;
 import africa.semicolon.toDoApplication.services.userService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.CREATED;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/toDoApplication")
@@ -25,6 +26,77 @@ public class UserController {
             return new ResponseEntity<>(new UserApiResponse(true, response), CREATED);
         }
         catch(TodoApplicationException e){
+            return new ResponseEntity<>(new UserApiResponse(false, e.getMessage()), BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/createTask")
+    public ResponseEntity<?> createTask(@RequestBody UserTaskCreationRequest userRegistrationRequest) {
+        try{
+            TaskCreationResponse response = userService.createTask(userRegistrationRequest);
+            return new ResponseEntity<>(new UserApiResponse(true, response), CREATED);
+        }
+        catch(TodoApplicationException e){
+            return new ResponseEntity<>(new UserApiResponse(false, e.getMessage()), BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/updateTask")
+    public ResponseEntity<?> updateTask(@RequestBody UserTaskUpdateRequest userTaskUpdateRequest) {
+        try{
+            UserTaskUpdateResponse userTaskUpdateResponse = userService.updateTask(userTaskUpdateRequest);
+            return new ResponseEntity<>(new UserApiResponse(true, userTaskUpdateResponse), CREATED);
+        } catch (TodoApplicationException e){
+            return new ResponseEntity<>(new UserApiResponse(false, e.getMessage()), BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody UserLoginRequest userLoginRequest){
+        try{
+            userService.loginUser(userLoginRequest);
+            return new ResponseEntity<>(new UserApiResponse(true, STR.process(StringTemplate.of("Login Successful"))), ACCEPTED);
+        } catch(TodoApplicationException e){
+            return new ResponseEntity<>(new UserApiResponse(false, e.getMessage()), BAD_REQUEST);
+        }
+    }
+
+    @PatchMapping("/logout/{id}")
+    public ResponseEntity<?> logout(@PathVariable("id") int id){
+        try{
+            userService.logoutUser(id);
+            return new ResponseEntity<>(new UserApiResponse(true, STR.process(StringTemplate.of("Logout Successful"))), ACCEPTED);
+        } catch(TodoApplicationException e){
+            return new ResponseEntity<>(new UserApiResponse(false, e.getMessage()), BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/updateUser")
+    public ResponseEntity<?> updateUser(@RequestBody UserUpdateRequest userUpdateRequest){
+        try{
+            UserUpdateResponse userUpdateResponse = userService.updateUser(userUpdateRequest);
+            return new ResponseEntity<>(new UserApiResponse(true, userUpdateResponse), CREATED);
+        } catch (TodoApplicationException e) {
+            return new ResponseEntity<>(new UserApiResponse(false, e.getMessage()), BAD_REQUEST);
+        }
+    }
+
+    @PatchMapping("/deleteUser/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable("id") int id){
+        try{
+            userService.deleteUser(id);
+            return new ResponseEntity<>(new UserApiResponse(true, STR.process(StringTemplate.of("User Deleted Successfully"))), ACCEPTED);
+        } catch (TodoApplicationException e){
+            return new ResponseEntity<>(new UserApiResponse(false, e.getMessage()), BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/deleteUser/{id}")
+    public ResponseEntity<?> getAllTask(@PathVariable("id") int id){
+        try{
+            List<Task> tasks = userService.getAllTasks(id);
+            return new ResponseEntity<>(new UserApiResponse(true, tasks), ACCEPTED);
+        } catch (TodoApplicationException e){
             return new ResponseEntity<>(new UserApiResponse(false, e.getMessage()), BAD_REQUEST);
         }
     }
