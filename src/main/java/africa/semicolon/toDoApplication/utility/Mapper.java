d7e08363b6e2d099bc6bc757ac9e645871bee9a2
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-23 10:16:44 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/utility/Mapper.java
+++ src/main/java/africa/semicolon/toDoApplication/utility/Mapper.java
@@ -2,10 +2,7 @@ package africa.semicolon.toDoApplication.utility;
 
 import africa.semicolon.toDoApplication.datas.models.*;
 import africa.semicolon.toDoApplication.dtos.request.*;
-import africa.semicolon.toDoApplication.dtos.response.TaskCreationResponse;
-import africa.semicolon.toDoApplication.dtos.response.UserRegistrationResponse;
-import africa.semicolon.toDoApplication.dtos.response.UserTaskUpdateResponse;
-import africa.semicolon.toDoApplication.dtos.response.UserUpdateResponse;
+import africa.semicolon.toDoApplication.dtos.response.*;
 
 import static africa.semicolon.toDoApplication.utility.Utility.mergeDateAndTime;
 
@@ -13,8 +10,8 @@ public class Mapper {
 
     public static Task map(TaskCreationRequest taskCreationRequest) {
         Task task = new Task();
-        task.setTitle(taskCreationRequest.getTitle());
-        task.setDescription(taskCreationRequest.getDescription());
+        task.setTitle(taskCreationRequest.getTitle().toLowerCase());
+        task.setDescription(taskCreationRequest.getDescription().toLowerCase());
         task.setStatus(Status.CREATED);
 
         Notification notification = taskCreationRequest.getNotification();
@@ -27,21 +24,12 @@ public class Mapper {
         return task;
     }
 
-    public static NotificationUpdateRequest map(TaskUpdateRequest taskUpdateRequest){
-        NotificationUpdateRequest notificationUpdateRequest = new NotificationUpdateRequest();
-        notificationUpdateRequest.setDate(taskUpdateRequest.getDueDate());
-        notificationUpdateRequest.setTime(taskUpdateRequest.getTime());
-        notificationUpdateRequest.setMessage(taskUpdateRequest.getMessage());
-        notificationUpdateRequest.setId(taskUpdateRequest.getNotificationId());
-        notificationUpdateRequest.setRead(taskUpdateRequest.isRead());
-        return notificationUpdateRequest;
-    }
 
 
 
     public static User map(UserRegistrationRequest userRegistrationRequest){
         User user = new User();
-        user.setUsername(userRegistrationRequest.getUsername());
+        user.setUsername(userRegistrationRequest.getUsername().toLowerCase());
         user.setEmail(userRegistrationRequest.getEmail());
         TaskList taskList = new TaskList();
         user.setTaskList(taskList);
@@ -81,20 +69,6 @@ public class Mapper {
         return taskCreationResponse;
     }
 
-    public static TaskUpdateRequest map(UserTaskUpdateRequest userTaskUpdateRequest) {
-        TaskUpdateRequest taskUpdateRequest = new TaskUpdateRequest();
-        taskUpdateRequest.setId(userTaskUpdateRequest.getTaskId());
-        taskUpdateRequest.setTitle(userTaskUpdateRequest.getTitle());
-        taskUpdateRequest.setDescription(userTaskUpdateRequest.getDescription());
-        taskUpdateRequest.setStatus(userTaskUpdateRequest.getStatus());
-        taskUpdateRequest.setMessage(userTaskUpdateRequest.getMessage());
-        taskUpdateRequest.setRead(userTaskUpdateRequest.isRead());
-        taskUpdateRequest.setPriority(userTaskUpdateRequest.getPriority());
-        taskUpdateRequest.setDueDate(userTaskUpdateRequest.getDueDate());
-        taskUpdateRequest.setTime(userTaskUpdateRequest.getTime());
-        taskUpdateRequest.setNotificationId(userTaskUpdateRequest.getNotificationId());
-        return taskUpdateRequest;
-    }
 
     public static UserUpdateResponse map(UserUpdateRequest userUpdateRequest) {
         UserUpdateResponse userUpdateResponse = new UserUpdateResponse();
@@ -104,21 +78,80 @@ public class Mapper {
         return userUpdateResponse;
     }
 
-    public static UserTaskUpdateResponse map(TaskUpdateRequest taskUpdateRequest, int userId){
-        UserTaskUpdateResponse userTaskUpdateResponse = new UserTaskUpdateResponse();
-        userTaskUpdateResponse.setTaskId(taskUpdateRequest.getId());
-        userTaskUpdateResponse.setTaskTitle(taskUpdateRequest.getTitle());
-        userTaskUpdateResponse.setUserId(userId);
-        return userTaskUpdateResponse;
-    }
-
     public static TaskCreationRequest map(TaskAssignmentRequest taskAssignment) {
         TaskCreationRequest taskCreationRequest = new TaskCreationRequest();
         taskCreationRequest.setNotificationMessage(taskAssignment.getNotificationMessage());
+        taskCreationRequest.setNotificationTime(taskAssignment.getNotificationTime());
         taskCreationRequest.setTitle(taskAssignment.getTitle());
         taskCreationRequest.setDueDate(taskAssignment.getDueDate());
         taskCreationRequest.setDescription(taskAssignment.getDescription());
         return taskCreationRequest;
+    }
+
+    public static AssignTaskToNewUserResponse map(String assignorUsername, String assigneeUsername, String title, int taskId, int userId) {
+        AssignTaskToNewUserResponse response = new AssignTaskToNewUserResponse();
+        response.setAssigneeUsername(assigneeUsername);
+        response.setAssignorUsername(assignorUsername);
+        response.setTaskName(title);
+        response.setTaskId(taskId);
+        response.setNewUserId(userId);
+        return response;
+    }
+
+    public static NotificationUpdateRequest map(TaskUpdateDueDateRequest taskUpdateDueDateRequest, int notificationId) {
+        NotificationUpdateRequest notificationUpdateRequest = new NotificationUpdateRequest();
+        notificationUpdateRequest.setId(notificationId);
+        notificationUpdateRequest.setDate(taskUpdateDueDateRequest.getDueDate());
+        return notificationUpdateRequest;
+    }
+
+    public static NotificationUpdateRequest map(TaskNotificationUpdateRequest taskNotificationUpdateRequest, Task task) {
+        NotificationUpdateRequest notificationUpdateRequest = new NotificationUpdateRequest();
+        notificationUpdateRequest.setId(task.getNotification().getId());
+        notificationUpdateRequest.setTime(taskNotificationUpdateRequest.getNotificationTime());
+        return notificationUpdateRequest;
+    }
+
+    public static TaskUpdateTitleRequest map(UserTaskTitleUpdateRequest userTaskTitleUpdateRequest) {
+        TaskUpdateTitleRequest taskUpdateTitleRequest = new TaskUpdateTitleRequest();
+        taskUpdateTitleRequest.setTitle(userTaskTitleUpdateRequest.getTitle().toLowerCase());
+        taskUpdateTitleRequest.setTaskId(userTaskTitleUpdateRequest.getTaskId());
+        return taskUpdateTitleRequest;
+    }
+
+    public static TaskUpdateDescriptionRequest map(UserTaskDescriptionUpdateRequest userTaskDescriptionUpdateRequest) {
+        TaskUpdateDescriptionRequest taskUpdateDescriptionRequest = new TaskUpdateDescriptionRequest();
+        taskUpdateDescriptionRequest.setTaskId(userTaskDescriptionUpdateRequest.getTaskId());
+        taskUpdateDescriptionRequest.setDescription(userTaskDescriptionUpdateRequest.getDescription().toLowerCase());
+        return taskUpdateDescriptionRequest;
+    }
+
+    public static TaskUpdatePriorityRequest map(UserTaskPriorityUpdateRequest userTaskPriorityUpdateRequest) {
+        TaskUpdatePriorityRequest taskUpdatePriorityRequest = new TaskUpdatePriorityRequest();
+        taskUpdatePriorityRequest.setPriorityNumber(userTaskPriorityUpdateRequest.getPriorityNumber());
+        taskUpdatePriorityRequest.setTaskId(userTaskPriorityUpdateRequest.getTaskId());
+        return taskUpdatePriorityRequest;
+    }
+
+    public static TaskUpdateDueDateRequest map(UserTaskDueDateUpdateRequest userTaskDueDateUpdateRequest) {
+        TaskUpdateDueDateRequest taskUpdateDueDateRequest = new TaskUpdateDueDateRequest();
+        taskUpdateDueDateRequest.setTaskId(userTaskDueDateUpdateRequest.getTaskId());
+        taskUpdateDueDateRequest.setDueDate(userTaskDueDateUpdateRequest.getDueDate());
+        return taskUpdateDueDateRequest;
+    }
+
+    public static TaskUpdateStatusRequest map(UserTaskStatusUpdateRequest userTaskStatusUpdateRequest) {
+        TaskUpdateStatusRequest taskUpdateStatusRequest = new TaskUpdateStatusRequest();
+        taskUpdateStatusRequest.setStatusNumber(userTaskStatusUpdateRequest.getStatusNumber());
+        taskUpdateStatusRequest.setTaskId(userTaskStatusUpdateRequest.getTaskId());
+        return taskUpdateStatusRequest;
+    }
+
+    public static TaskNotificationUpdateRequest map(UserNotificationTimeUpdateRequest userNotificationTimeUpdateRequest) {
+        TaskNotificationUpdateRequest taskNotificationUpdateRequest = new TaskNotificationUpdateRequest();
+        taskNotificationUpdateRequest.setTaskId(userNotificationTimeUpdateRequest.getTaskId());
+        taskNotificationUpdateRequest.setNotificationTime(userNotificationTimeUpdateRequest.getNotificationTime());
+        return taskNotificationUpdateRequest;
     }
 
 }
