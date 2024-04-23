bf7d02c2140a6f5ad60b0cfc5d9500ed876e527f
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-23 10:15:04 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/services/taskService/TaskService.java
+++ src/main/java/africa/semicolon/toDoApplication/services/taskService/TaskService.java
@@ -1,17 +1,22 @@
 package africa.semicolon.toDoApplication.services.taskService;
 
 import africa.semicolon.toDoApplication.datas.models.Task;
-import africa.semicolon.toDoApplication.dtos.request.TaskCreationRequest;
-import africa.semicolon.toDoApplication.dtos.request.TaskUpdateRequest;
+import africa.semicolon.toDoApplication.dtos.request.*;
 
 import java.util.List;
 
 public interface TaskService {
 
     Task createTask(TaskCreationRequest taskCreationRequest);
-    void updateTask(TaskUpdateRequest taskUpdateRequest);
+    void updateTaskTitle(TaskUpdateTitleRequest taskUpdateTitleRequest);
+    void updateTaskDescription(TaskUpdateDescriptionRequest taskUpdateDescriptionRequest);
+    void updateTaskPriority(TaskUpdatePriorityRequest taskUpdatepriorityRequest);
+    void updateTaskDueDate(TaskUpdateDueDateRequest taskUpdateDueDateRequest);
+    void updateTaskStatus(TaskUpdateStatusRequest taskUpdateStatusRequest);
+    void updateTaskNotificationTime(TaskNotificationUpdateRequest taskNotificationUpdateRequest);
     void deleteTask(int id);
     Task searchForTaskById(int id);
     void save(Task task);
     List<Task> getAllTasks();
+
 }
