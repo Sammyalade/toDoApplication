2861d60ec1647995215117e9b3c3dd9814d49c02
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-23 10:09:59 +0100

    Commit

--- src/main/java/africa/semicolon/toDoApplication/exceptions/EmptyStringException.java
+++ src/main/java/africa/semicolon/toDoApplication/exceptions/EmptyStringException.java
@@ -1,4 +1,4 @@
-package africa.semicolon.toDoApplication.exception;
+package africa.semicolon.toDoApplication.exceptions;
 
 public class EmptyStringException extends TodoApplicationException{
     public EmptyStringException(String message) {
