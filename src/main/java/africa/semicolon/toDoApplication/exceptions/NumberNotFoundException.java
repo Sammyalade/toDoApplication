dbd93eead8e2c86ddd26d979a3a762b4a3352933
Sammyalade <olufemialadeyelu@gmail.com>
2024-04-23 10:11:14 +0100

    Commit

---
+++ src/main/java/africa/semicolon/toDoApplication/exceptions/NumberNotFoundException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.toDoApplication.exceptions;
+
+public class NumberNotFoundException extends TodoApplicationException{
+    public NumberNotFoundException(String message) {
+        super(message);
+    }
+}
