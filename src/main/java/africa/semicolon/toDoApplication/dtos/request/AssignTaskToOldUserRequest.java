6bdbdb6574fad7f84a356b67a3b6d25b390639f6
Sammyalade <olufemialadeyelu@gmail.com>
2024-05-01 11:57:32 +0100

    Commit

---
+++ src/main/java/africa/semicolon/toDoApplication/dtos/request/AssignTaskToOldUserRequest.java
@@ -0,0 +1,19 @@
+package africa.semicolon.toDoApplication.dtos.request;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+public class AssignTaskToOldUserRequest {
+
+    private String assignorUsername;
+    private String assignorEmail;
+    private String assigneeEmail;
+    private String title;
+    private String description;
+    private LocalDate dueDate;
+    private String notificationMessage;
+    private LocalTime notificationTime;
+}
